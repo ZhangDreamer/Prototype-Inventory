d1ee10db9339fa6f6e5c691b3e7d2c78e2da3ea6
ZhangDreamer <anyelzhang@gmail.com>
2023-03-01 20:52:25 -0500

    Add initial code for Prototype Inventory project

---
+++ user/views.py
@@ -0,0 +1,47 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm
+from django.http import HttpResponse
+from .forms import CreateUserForm, UserUpdateForm, ProfileUpdateForm
+from django.contrib import messages
+
+# Create your views here.
+def register(request):
+    if request.method == 'POST':
+        form = CreateUserForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            messages.success(request, f'{username} account has been created. Proceed to login.')
+            return redirect('user-login')
+        else:
+            # Add errors to the form as a whole
+            form.add_error(None, "There was an error with your submission. Please try again.")
+            return render(request, 'user/register.html', context={'form':form})
+    else:
+        form = CreateUserForm()
+        context = {
+            'form':form,
+        }
+        return render(request, 'user/register.html', context={'form':form})
+
+def profile(request):
+    return render(request, 'user/profile.html')
+
+def profile_update(request):
+    if request.method=="POST":
+        user_form = UserUpdateForm(request.POST, instance=request.user)
+        profile_form = ProfileUpdateForm(request.POST, request.FILES, instance=request.user.profile)
+        if user_form.is_valid() and profile_form.is_valid():
+            user_form.save()
+            profile_form.save()
+            return redirect('user-profile')
+    else:
+        user_form = UserUpdateForm(instance=request.user)
+        profile_form = ProfileUpdateForm(instance=request.user.profile)
+    
+    context={
+        'user_form' : user_form,
+        'profile_form' : profile_form
+    }
+    return render(request, 'user/profile_update.html', context)
+    
